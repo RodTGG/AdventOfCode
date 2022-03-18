a29857635befaf92d0297de42a67caa6ae2848c0
Rodrigo Sanchez <rodrigoasg36@outlook.com>
2022-03-18 19:52:09 +1100

    tmp

--- 2021/Day3/main.cpp
+++ 2021/Day3/main.cpp
@@ -13,11 +13,6 @@
 
 #define nBits 12
 
-enum class FilterType {
-  Common,
-  LeastCommon
-};
-
 void Part1(const std::vector<std::bitset<nBits>> &data)
 {
   std::bitset<nBits> gammaBits{};
@@ -46,107 +41,42 @@ void Part1(const std::vector<std::bitset<nBits>> &data)
 
 void Part2(const std::vector<std::bitset<nBits>> &data)
 {
-  std::bitset<nBits> oxygen{};
-  std::bitset<nBits> co2{};
-
-  spdlog::info("Part 2!\n");
-
-  std::vector<std::bitset<nBits>> tmp = data;
-
-  for (auto i = 11; i > -1; --i) {
-    int bitCount{ 0 };
-    bool mostCommonBit{ false };
-
-    if (tmp.size() <= 1)
-      break;
-
-    bitCount = std::count_if(tmp.begin(), tmp.end(), [i](const std::bitset<nBits> &bit) {
-      return bit[i] == true;
-    });
-
-    mostCommonBit = bitCount >= static_cast<int>(((tmp.size() + 2 - 1) / 2));
-
-    tmp.erase(std::remove_if(tmp.begin(), tmp.end(), [i, mostCommonBit](const std::bitset<nBits> &bit) { return bit[i] != mostCommonBit; }), tmp.end());
-  }
-
-  oxygen = tmp.front();
-  tmp = data;
-
-  for (auto i = 11; i > -1; --i) {
-    int bitCount{ 0 };
-    bool leastCommonBit{ false };
-
-    if (tmp.size() <= 1)
-      break;
-
-    bitCount = std::count_if(tmp.begin(), tmp.end(), [i](const std::bitset<nBits> &bit) {
-      return bit[i] == true;
-    });
-
-    if (bitCount == 1)
-      leastCommonBit = false;
-    else
-      leastCommonBit = bitCount <= static_cast<int>(((tmp.size() + 2 - 1) / 2));
-
-    tmp.erase(std::remove_if(tmp.begin(), tmp.end(), [i, leastCommonBit](const std::bitset<nBits> &bit) { return bit[i] != leastCommonBit; }), tmp.end());
-  }
-
-  co2 = tmp.front();
-
-  spdlog::info("Oxygen: {0}", oxygen.to_string());
-  spdlog::info("C02: {0}", co2.to_string());
-  spdlog::info("Total: {0}\n", oxygen.to_ulong() * co2.to_ulong());
-}
-
-std::vector<std::bitset<nBits>> filter_data(const std::vector<std::bitset<nBits>> &data, FilterType filter = FilterType::Common)
-{
-  std::vector<std::bitset<nBits>> result{ data };  
-
-  return result;
-}
-
-void test()
-{
-  const int bits = nBits;// nBits bit numbers
-
-  std::vector<std::bitset<bits>> diagnostics;
-  std::ifstream fin("input.txt");
-  std::string line;
-
-  while (fin >> line) {
-    diagnostics.push_back(std::bitset<bits>(line));
-  }
+  const int bits = nBits;
 
   std::bitset<bits> oxygen_generator_rating;
+  std::vector<std::bitset<bits>> most_common(data);
 
-  std::vector<std::bitset<bits>> most_common(diagnostics);// avoid changing input by duplicating
-  for (int i = bits - 1; most_common.size() > 1 && i >= 0; i--) {
+  for (int i = bits - 1; most_common.size() > 1 && i >= 0; i--) 
+  {
+
     std::array<std::vector<std::bitset<bits>>, 2> filtered;
-    for (auto diagnostic : most_common) {
+    for (auto diagnostic : most_common) 
       filtered[diagnostic[i]].push_back(diagnostic);
-    }
+
     most_common = filtered[0].size() > filtered[1].size() ? filtered[0] : filtered[1];
-    //if equal, keep 1
   }
+
   oxygen_generator_rating = most_common.at(0);
 
   std::bitset<bits> co2_scrubber_rating;
+  std::vector<std::bitset<bits>> least_common(data);
 
-  std::vector<std::bitset<bits>> least_common(diagnostics);//avoid changing input by duplicating
-  for (int i = bits - 1; least_common.size() > 1 && i >= 0; i--) {
+  for (int i = bits - 1; least_common.size() > 1 && i >= 0; i--) 
+  {
+
     std::array<std::vector<std::bitset<bits>>, 2> filtered;
-    for (auto diagnostic : least_common) {
+
+    for (auto diagnostic : least_common) 
       filtered[diagnostic[i]].push_back(diagnostic);
-    }
+    
     least_common = filtered[0].size() > filtered[1].size() ? filtered[1] : filtered[0];
-    // if equal, keep 0
   }
+
   co2_scrubber_rating = least_common.at(0);
 
-  // convert to unsigned long int
-  std::cout << "Oxygen generator rating : " << oxygen_generator_rating.to_ulong() << std::endl;
-  std::cout << "Co2 scrubber rating : " << co2_scrubber_rating.to_ulong() << std::endl;
-  std::cout << "Life support rating : " << oxygen_generator_rating.to_ulong() * co2_scrubber_rating.to_ulong() << std::endl;
+  spdlog::info("Oxygen generator rating: {0}", oxygen_generator_rating.to_ulong());
+  spdlog::info("Co2 scrubber ratin: {0}", co2_scrubber_rating.to_ulong());
+  spdlog::info("Life support rating: {0}", oxygen_generator_rating.to_ulong() * co2_scrubber_rating.to_ulong());
 }
 
 int main(int, char **)
@@ -171,7 +101,6 @@ int main(int, char **)
 
   Part1(data);
   Part2(data);
-  test();
 
   return 0;
 }
