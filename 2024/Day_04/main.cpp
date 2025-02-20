9bc4e29badc2663ef0a9d972a91447622e79a5be
Rodrigo Sanchez <rodrigosg36@gmail.com>
2025-02-20 21:17:27 +1100

    Added day 4

--- 2024/Day_04/main.cpp
+++ 2024/Day_04/main.cpp
@@ -3,23 +3,109 @@
 #include <fmt/base.h>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <numeric>
-#include <ranges>
-#include <regex>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
-void Part_1(const std::vector<std::string> data) {
+using Grid = std::vector<std::vector<char>>;
+
+void Part_1(const std::string &word, const Grid &grid) {
   fmt::println("---------------- Part 1 ----------------");
 
+  if (word.empty() || grid.empty())
+    return;
 
+  auto result = 0;
+  auto gW = grid[0].size();
+  auto gH = grid.size();
+
+  for (auto y = 0; y < gH; ++y) {
+    for (auto x = 0; x < gW; ++x) {
+      if (grid[y][x] != word.front())
+        continue;
+
+      for (auto dY = -1; dY < 2; ++dY) {
+        for (auto dX = -1; dX < 2; ++dX) {
+          if (dY == 0 && dX == 0)
+            continue;
+
+          for (auto wC = 1; wC < word.length(); ++wC) {
+            auto tmpY = y + dY * wC;
+            auto tmpX = x + dX * wC;
+
+            if (tmpY < 0 || tmpY >= gH || tmpX < 0 || tmpX >= gW)
+              break;
+            else if (word[wC] != grid[tmpY][tmpX])
+              break;
+            else if (wC == word.length() - 1)
+              result++;
+          }
+        }
+      }
+    }
+  }
+
+  fmt::println("Result: {}", result);
+}
+
+void Part_2(const Grid &grid) {
+
+  fmt::println("---------------- Part 2 ----------------");
+
+  auto result = 0;
+
+  auto gW = grid[0].size();
+  auto gH = grid.size();
+
+  std::vector<std::pair<int, int>> directions{{-1, -1},
+                                              {
+                                                  1,
+                                                  1,
+                                              },
+                                              {-1, 1},
+                                              {1, -1}};
+
+  for (auto y = 0; y < gH; ++y) {
+    for (auto x = 0; x < gW; ++x) {
+
+      if (grid[y][x] != 'A')
+        continue;
+
+      auto boundCheck = [&](const int dY, const int dX) {
+        if (dY < 0 || dY >= gH || dX < 0 || dX >= gW)
+          return false;
+
+        return true;
+      };
+
+      auto checkDiagonal = [&](const std::pair<int, int> &diagA,
+                               const std::pair<int, int> &diagB) {
+        if (!boundCheck(y + diagA.first, x + diagA.second) ||
+            !boundCheck(y + diagB.first, x + diagB.second))
+          return false;
+
+        return (grid[y + diagA.first][x + diagA.second] == 'M' &&
+                grid[y + diagB.first][x + diagB.second] == 'S') ||
+               (grid[y + diagA.first][x + diagA.second] == 'S' &&
+                grid[y + diagB.first][x + diagB.second] == 'M');
+      };
+
+      auto lD = checkDiagonal(directions[0], directions[1]);
+      auto rD = checkDiagonal(directions[2], directions[3]);
+
+      if (!lD || !rD)
+        continue;
+
+      result++;
+    }
+  }
+
+  fmt::println("Result: {}", result);
 }
 
 int main(int argc, char *argv[]) {
 
-  fmt::println("Advent Of Code 2024 - Day 3");
+  fmt::println("Advent Of Code 2024 - Day 4");
 
   std::ifstream dataFile{"input.txt", std::ios::binary};
 
@@ -30,18 +116,21 @@ int main(int argc, char *argv[]) {
 
   const auto fSize = std::filesystem::file_size("input.txt");
 
-  std::string fileBuffer(fSize, '\0');
-  dataFile.read(fileBuffer.data(), fSize);
+  std::string memory(fSize, '\0');
+  dataFile.read(memory.data(), fSize);
   dataFile.close();
 
-  std::vector<std::string> data{};
-  std::istringstream stream(fileBuffer);
+  Grid dataGrid{};
+  std::istringstream stream(memory);
   std::string line{};
 
-  while (std::getline(stream, line))
-        data.emplace_back(line);
+  while (std::getline(stream, line)) {
+    std::vector<char> cv(line.begin(), line.end());
+    dataGrid.emplace_back(cv);
+  }
 
-  Part_1(data);
+  Part_1("XMAS", dataGrid);
+  Part_2(dataGrid);
 
   return 0;
 }
